0b84417b52693bca906d1ae71986497000323cd1
Pierre Leroy <pierrotleroy@hotmail.fr>
2018-05-23 11:47:50 +0200

    resolve 2

--- consts.php
+++ consts.php
@@ -6,11 +6,5 @@
 	define('NAME', '');
 	define('WIKI_NAME', '');
 	define('PUBLISH', false);
-<<<<<<< HEAD
 	// list of parent tasks that isn't useful (so the real parent would be a child)
 	$parentToIgnore = array();
-=======
-
-	// list of parent tasks that isn't useful (so the real parent would be a child)
-	$parentToIgnore = array();
->>>>>>> add315aa3d7a2a866a4d23ef3d2fb35701987e26
