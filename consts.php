5b62dc32ec3caa31cc2bbbd97b115ba301adf493
icfeavoir <pierrotleroy@hotmail.fr>
2018-05-23 10:52:20 +0200

    Ajout de la const PUBLISH

--- consts.php
+++ consts.php
@@ -5,6 +5,7 @@
 	define('USER_ID', '');
 	define('NAME', '');
 	define('WIKI_NAME', '');
+	define('PUBLISH', false);
 
 	// list of parent tasks that isn't useful (so the real parent would be a child)
 	$parentToIgnore = array(
